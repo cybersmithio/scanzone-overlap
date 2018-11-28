49614465f8d210bd8b05c3304a83045e81d62456
jamessmith <jamessmith>
2018-11-28 09:00:05 -0500

    Updated to work with Python 3

--- scanzone-overlaps.py
+++ scanzone-overlaps.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # Written by: James Smith
-# Version: 0.9 
+# Version: 1.0
 # Created: June 13, 2017
+#
+# Version 1.0 - confirmed working with Python 3.x
+# Version 0.9 - initial version.  Only worked with Python 2.x
+#
+#
 #
 # This is a concept script meant to see if any scan zones in SecurityCenter have an overlap.
 #
@@ -22,6 +27,7 @@ import json
 import netaddr
 import string
 import os
+import argparse
 
 ###############################################
 # CODE BELOW HERE - NOTHING TO CHANGE BY USER #
@@ -37,7 +43,7 @@ def checkScanZones(sc):
 
 	#Iterate through all the scan zones and download the IP ranges
 	for i in resp.json()['response']:
-		print "Examining scan zone \""+i['name']+"\""
+		print("Examining scan zone \""+i['name']+"\"")
 		#print "id",i['id']
 		resp=sc.get('zone/'+str(i['id'])+'?fields=name%2Cdescription%2CipList%2CcreatedTime%2Cranges%2Cscanners%2Cname%2Cscanners%2CtotalScanners%2CactiveScanners%2CtotalScanners%2CmodifiedTime%2CcanUse%2CcanManage')
 		iplist=resp.json()['response']['ipList'].split(',')
@@ -45,7 +51,7 @@ def checkScanZones(sc):
 			#print "IP Range in scan zone",j
 
 			#Check if the IP address is an IP range (instead of a single IP or CIDR)
-			hyphen=string.find(j,"-")
+			hyphen=j.find("-")
 			if( hyphen >= 0 ):
 				#If the IP address is a range, convert it to CIDR notation
 				#print "CIDRs",netaddr.iprange_to_cidrs(j[0:hyphen],j[hyphen+1:])
@@ -63,7 +69,7 @@ def checkScanZones(sc):
 		for j in range(i+1,len(scanzoneranges)):
 			n2=ipaddr.IPNetwork(scanzoneranges[j][0])
 			if n1.overlaps(n2):
-				print n1,"in scan zone \""+str(scanzoneranges[i][1]['name'])+"\" overlaps with",n2,"in scan zone \""+str(scanzoneranges[j][1]['name'])+"\""
+				print(str(n1)+" in scan zone \""+str(scanzoneranges[i][1]['name'])+"\" overlaps with "+str(n2)+" in scan zone \""+str(scanzoneranges[j][1]['name'])+"\"")
 
 	return(True)
 
@@ -76,11 +82,27 @@ def checkScanZones(sc):
 ### Program start
 ###
 ######################
-# Look for SecurityCenter login information
+
+# Look for SecurityCenter login information from the CLI variables.
+parser = argparse.ArgumentParser(description="Check for scan zone overlaps in a Tenable SecurityCenter installation.")
+parser.add_argument('--host',help="The SecurityCenter host IP address",nargs=1,action="store")
+parser.add_argument('--port',help="The SecurityCenter port number",nargs=1,action="store")
+parser.add_argument('--username',help="The SecurityCenter admin username",nargs=1,action="store")
+parser.add_argument('--password',help="The SecurityCenter admin password.  (Not recommended on command line, use environment variable)",nargs=1,action="store")
+args=parser.parse_args()
+
+
+# Look for SecurityCenter login information from the environment variables.
 if os.getenv('SCHOST') is None:
 	schost = ""
 else:
 	schost = os.getenv('SCHOST')
+
+# Look for SecurityCenter login information from the environment variables.
+if os.getenv('SCPORT') is None:
+	scport = "443"
+else:
+	scport = os.getenv('SCPORT')
 
 if os.getenv('SCUSERNAME') is None:
 	username = ""
@@ -92,19 +114,34 @@ if os.getenv('SCPASSWORD') is None:
 else:
 	password = os.getenv('SCPASSWORD')
 
+if args.host[0] != "":
+	schost=args.host[0]
+if args.port[0] != "":
+	scport=args.port[0]
+if args.username[0] != "":
+	username=args.username[0]
+if args.password[0] != "":
+	password=args.password[0]
+
+
+
+
+if schost == "":
+	print("No SecurityCenter host specified.  Please supply in the environment variables or command line.")
+
 # Create a session as the user
 try:
-	scconn = SecurityCenter5(schost)
+	scconn = SecurityCenter5(schost,port=scport)
 except:
-	print "Unable to connect to SecurityCenter"
-	print("Make sure to set SCHOST, SCUSERNAME, and SCPASSWORD environment variables and export them.")
+	print("Unable to connect to SecurityCenter")
+	print("Make sure to set SCHOST, SCUSERNAME, and SCPASSWORD environment variables and export them.  Optionally there is an SCPORT variable as well.")
 	exit(-1)
 
 try:
 	scconn.login(username, password)
 except:
-	print "Unable to log into SecurityCenter"
-	print("Make sure to set SCHOST, SCUSERNAME, and SCPASSWORD environment variables and export them.")
+	print("Unable to log into SecurityCenter")
+	print("Make sure to set SCHOST, SCUSERNAME, and SCPASSWORD environment variables and export them.  Optionally there is an SCPORT variable as well.")
 
 print("Logged into SecurityCenter")
 checkScanZones(scconn)
